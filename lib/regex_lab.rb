926ddb1885bd8bb19e8784bd341d9b692a8d6b19
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-06 14:26:47 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,5 @@
 def starts_with_a_vowel?(word)
-    if word.match(/\b+[aeiouAEIOU]/)
-      return true
-    end
-    return false
+   word.match(/\b+[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -14,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match
 end
 
 def valid_phone_number?(phone)
