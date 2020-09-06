ccd921b7c1de2aede81d5a4645840aab7863ac79
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-06 14:43:51 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,7 +21,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.grep(/\b[0-9]{10}\b/)
+  if phone.grep(/[0-9]{10}/)
     return true
   end
   return false
