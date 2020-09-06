5d2445c878d745ae8510f55c824b6c1c51deccb9
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-06 14:24:32 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,18 @@
 def starts_with_a_vowel?(word)
-
+    if word.slice(0).match(/[aeiouAEIOU]/)
+      return true
+    end
+    return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  result = text.scan(/\b\w{5}\b/)
+  puts result
+  return result
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
